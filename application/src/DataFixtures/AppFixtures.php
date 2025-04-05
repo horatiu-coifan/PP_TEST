85a41db8943e924770df1ad132a261abcbd60039
MBP-I5 <oldie@HCss-MacBook-Pro.local>
2025-04-05 09:34:51 +0300

    update_fixtures

--- application/src/DataFixtures/AppFixtures.php
+++ application/src/DataFixtures/AppFixtures.php
@@ -21,51 +21,26 @@ class AppFixtures extends Fixture
         // $manager->flush();
 
         // Add counties
-        $bucuresti = $this -> addCounty($manager, "Bucuresti");
-        $ilfov = $this -> addCounty($manager, "Ilfov");
-        $brasov = $this -> addCounty($manager, "Brasov");
-        $cluj = $this -> addCounty($manager, "Cluj");
-        $constanta = $this -> addCounty($manager, "Constanta");
-        $timisoara = $this -> addCounty($manager, "Timisoara");
-
-        // Add products
-        $prod1 = $this -> addProduct( $manager, "Product 1", "Description Product 1");
-        $prod2 = $this -> addProduct( $manager, "Product 2", "Description Product 2");
-        $prod3 = $this -> addProduct( $manager, "Product 3", "Description Product 3");
-        $prod4 = $this -> addProduct( $manager, "Product 4", "Description Product 4");
-        $prod5 = $this -> addProduct( $manager, "Product 5", "Description Product 5");
-        $prod6 = $this -> addProduct( $manager, "Product 6", "Description Product 6");
-        $prod7 = $this -> addProduct( $manager, "Product 7", "Description Product 7");
-
-        // Add clients
-        $client1 = $this -> addClient($manager, 'RO 12345678', 'Client 1 SRL', 'Str. Virtutii Nr.12', 'Bucuresti', $bucuresti);
-        $client2 = $this -> addClient($manager, 'RO 22233344', 'Client 2 SA', 'Str. Bunastarii Nr. 7', 'Bucuresti', $bucuresti);
-        $client3 = $this -> addClient($manager, '98762345', 'Client 3 SRL', 'Str. Mihai Viteazul Nr. 10', 'Brasov', $brasov);
+        $this -> addCounty($manager, "Bucuresti");
+        $this -> addCounty($manager, "Ilfov");
+        $this -> addCounty($manager, "Brasov");
+        $this -> addCounty($manager, "Cluj");
+        $this -> addCounty($manager, "Constanta");
+        $this -> addCounty($manager, "Timisoara");
+        $this -> addCounty($manager, "Iasi");
 
         // Add credentials
-        $this -> addCredentials($manager, $client1, 'user1', 'pass1', 'A', 'admin');
-        $this -> addCredentials($manager, $client2, 'user2', 'pass2', 'A', 'user');
-        $this -> addCredentials($manager, $client3, 'user3', 'pass3', 'A', 'user');
-
-        // Add transactions
-        $this -> addTransactions($manager, $client1, $prod1, 100);
-        $this -> addTransactions($manager, $client1, $prod2, 50);
-        $this -> addTransactions($manager, $client1, $prod3, 25);
-
-        $this -> addTransactions($manager, $client2, $prod1, 111.22);
-        $this -> addTransactions($manager, $client2, $prod4, 22.3);
-        $this -> addTransactions($manager, $client2, $prod5, 33.44);
-        $this -> addTransactions($manager, $client2, $prod6, 44.98);
-
-        $this -> addTransactions($manager, $client3, $prod7, 20.50);
-        $this -> addTransactions($manager, $client3, $prod5, 30.25);
+        $this -> addCredentials($manager,  'admin', 'admin', 'A', 'admin');
+        $this -> addCredentials($manager,  'user1', 'pass1', 'A', 'user');
+        $this -> addCredentials($manager,  'user2', 'pass2', 'A', 'user');
+        $this -> addCredentials($manager,  'user3', 'pass3', 'A', 'user');
     }
 
     protected function addCredentials(
             ObjectManager &$manager, 
             string $username, 
             string $password, 
-            string $status='A',
+            string $status,
             string $type
         ): void{
         try{
@@ -84,38 +59,6 @@ class AppFixtures extends Fixture
         }
     }
 
-    protected function addClient(
-            ObjectManager &$manager, 
-            string $cui,
-            string $name,
-            string $address,
-            string $city,
-            County $county
-        ): Clients{
-        try{
-            $client = new Clients();
-            $client
-                -> setCui($cui)
-                -> setName($name)
-                -> setAddress($address)
-                -> setCity($city)
-                -> setCounty($county)
-                -> setInsDate(new \DateTime())
-                -> setInsUid('admin')
-                -> setModDate(new \DateTime())
-                -> setModUid('admin')
-                -> setDeleted(false)
-            ;
-            $manager -> persist($client);
-            $manager -> flush();
-
-            return $client;
-        }
-        catch( Exception $exception){
-            throw new \ErrorException("Cannot insert client");
-        }
-    }
-
     protected function addCounty(
         ObjectManager &$manager, 
         string $name
@@ -130,58 +73,6 @@ class AppFixtures extends Fixture
         }
         catch( Exception $exception){
             throw new \ErrorException("Cannot insert county");
-        }
-    }
-
-    protected function addProduct(
-        ObjectManager &$manager, 
-        string $name,
-        string $description = ''
-    ): Products{
-        try{
-            $product = new Products();
-            $product
-                -> setName($name)
-                -> setDescription($description);
-            $manager -> persist($product);
-            $manager -> flush();
-
-            return $product;
-        }
-        catch( Exception $exception){
-            throw new \ErrorException("Cannot insert product");
-        }
-    }
-
-    protected function addTransactions(
-        ObjectManager &$manager, 
-        Clients $client,
-        Products $product,
-        string $amount,
-        string $date = '',
-        bool $status = false
-    ): Transactions{
-        try{
-            $transaction = new Transactions();
-            $transaction
-                -> setClient($client)
-                -> setProduct($product)
-                -> setAmount($amount)
-                -> setDate($date ?: new \DateTime())
-                -> setStatus($status)
-                -> setInsDate(new \DateTime())
-                -> setInsUid('admin')
-                -> setModDate(new \DateTime())
-                -> setModUid('admin')
-                -> setDeleted(0)
-            ;
-            $manager -> persist($transaction);
-            $manager -> flush();
-
-            return $transaction;
-        }
-        catch( Exception $exception){
-            throw new \ErrorException('Cannot insert transaction');
         }
     }
 }
